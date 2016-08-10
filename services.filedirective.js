518084d695a269abdc3e04d74e1fd043d733caab
FelixLA <4felix@gmail.com>
2016-08-10 08:28:51 -0700

    Add files via upload

---
+++ services.filedirective.js
@@ -0,0 +1,42 @@
+﻿
+(function () {
+
+    angular.module(APPNAME)
+    .directive('file', FileDirective);
+
+
+    function FileDirective() {
+        return {
+            scope: {
+                file: '='
+            },
+
+            link: function (scope, el, attrs) {
+
+                el.bind('change', function (event) {
+                    var files = event.target.files;
+                    var file = files[0];
+                    scope.file = file ? file.name : undefined;
+                    scope.$apply();
+                });
+            }
+        };
+    }
+})();
+
+(function () {
+    angular.module(APPNAME)
+    .directive('clickedDisable', function () {
+        return {
+            restrict: 'A',
+            link: function (scope, ele, attrs) {
+                $(ele).click(function () {
+                    $(ele).attr('disabled', true);
+                    $(ele).addClass('btn-success');
+                    $(ele).removeClass('btn-default');
+                    $(ele).text('Sent');
+                });
+            }
+        };
+    })
+})();
