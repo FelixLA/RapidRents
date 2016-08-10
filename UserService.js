d4db89a34446a3bf4889151697cf415342824ede
FelixLA <4felix@gmail.com>
2016-08-10 08:35:30 -0700

    Add files via upload

---
+++ UserService.js
@@ -0,0 +1,16 @@
+﻿(function () {
+    "use strict"
+
+    angular.module(APPNAME)
+         .factory('$userService', UserService);
+
+    UserService.$inject = ['$baseService'];
+
+    function UserService($baseService) {
+        var aSabioServiceObject = sabio.services.users;
+
+        var newService = $baseService.merge(true, {}, aSabioServiceObject, $baseService);
+
+        return newService;
+    };
+})();
